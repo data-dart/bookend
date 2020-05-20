87f525f4d0c8f2c7aca5b1c9aea379cc96fb8e21
Elaad Applebaum <elaad.applebaum@gmail.com>
2020-05-20 11:37:26 -0500

    PEP8ify, new tests

--- tests/test_book.py
+++ tests/test_book.py
@@ -1,41 +1,68 @@
 import pytest
 from src.book import BookText
 
+
 def test_read_file():
-	booktest = BookText('example_text.txt')
-	assert 1
+    booktest = BookText('example_text.txt')
+    assert 1
+
 
 def test_read_rawtext():
-	booktest = BookText(rawtext='Easy raw text')
-	assert 1
+    booktest = BookText(rawtext='Easy raw text')
+    assert 1
+
 
 @pytest.fixture
 def bt():
-	bookobj = BookText('example_text.txt')
-	return bookobj
+    bookobj = BookText('example_text.txt')
+    return bookobj
+
 
 def test_toc(bt):
-	assert bt.toc.startswith('Chap 1')
-	assert bt.toc.endswith('Chap 3')
+    assert bt.toc.startswith('Chap 1')
+    assert bt.toc.endswith('Chap 3')
+
 
 def test_scrape_meta(bt):
-	assert bt.author == 'Testy McTestface'
+    assert bt.author == 'Testy McTestface'
+
 
 def test_infer_meta(bt):
-	assert bt.title == 'text'
+    assert bt.title == 'text'
+
 
 def test_word_token_remstop_no_punct(bt):
-	assert len(bt.tokenize('word', rem_stopwords=True, 
-		include_punctuation=False)) == 7
+    assert len(bt.tokenize('word', rem_stopwords=True,
+                           include_punctuation=False)) == 7
+
 
 def test_word_token_remstop_yes_punct(bt):
-	assert len(bt.tokenize('word', rem_stopwords=True, 
-		include_punctuation=True)) == 14
+    assert len(bt.tokenize('word', rem_stopwords=True,
+                           include_punctuation=True)) == 14
+
 
 def test_word_token_no_punct(bt):
-	assert len(bt.tokenize('word', rem_stopwords=False, 
-		include_punctuation=False)) == 19
+    assert len(bt.tokenize('word', rem_stopwords=False,
+                           include_punctuation=False)) == 19
+
 
 def test_word_token_yes_punct(bt):
-	assert len(bt.tokenize('word', rem_stopwords=False, 
-		include_punctuation=True)) == 26
+    assert len(bt.tokenize('word', rem_stopwords=False,
+                           include_punctuation=True)) == 26
+
+
+def test_snippet_on_sent(bt):
+    snip = bt.snippet(2, 'sent')
+    assert len(snip.tokenize('sent')) == 2
+
+
+def test_snippet_on_word(bt):
+    snip = bt.snippet(4, 'sent')
+    assert len(snip.tokenize('word')) == 4
+
+
+def test_snippet_char_continuity(bt):
+    snip = bt.snippet(12, 'char', non_cont=True)
+    assert snip.text not in bt.text
+    snip = bt.snippet(12, 'char', non_cont=False)
+    assert snip.text in bt.text
